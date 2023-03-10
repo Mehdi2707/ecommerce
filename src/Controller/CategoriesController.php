d0689fc6459edcc91588a2d5d9609f8746f7033d
Mehdi2707 <mehdi2707@live.fr>
2023-03-10 15:28:10 +0100

    fix error

---
+++ src/Controller/CategoriesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categories;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/categories', name: 'categories_')]
+class CategoriesController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('products/index.html.twig', [
+            'controller_name' => 'ProductsController',
+        ]);
+    }
+    
+    #[Route('/{slug}', name: 'list')]
+    public function list(Categories $categories): Response
+    {
+        return $this->render('categories/list.html.twig', [
+            'category' => $categories,
+        ]);
+    }
+}
