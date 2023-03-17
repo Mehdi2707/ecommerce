0e042b0fb3269c020200b0dcdd6eb2fe5d464c22
Mehdi2707 <mehdi2707@live.fr>
2023-03-17 16:38:48 +0100

    modification user et mot de passe

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\UsersFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/profil', name: 'profile_')]
@@ -13,7 +17,53 @@ class ProfileController extends AbstractController
     public function index(): Response
     {
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+
+        ]);
+    }
+
+    #[Route('/edition', name: 'edit')]
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UsersFormType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Profil mis à jour');
+            return $this->redirectToRoute('profile_index');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/edition/pass', name: 'edit_password')]
+    public function editPassword(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        if($request->isMethod('POST'))
+        {
+            $user = $this->getUser();
+
+            if($request->request->get('pass') == $request->request->get('pass2'))
+            {
+                $user->setPassword($passwordHasher->hashPassword($user, $request->request->get('pass')));
+                $entityManager->persist($user);
+                $entityManager->flush();
+                $this->addFlash('success', 'Mot de passe mis à jour avec succès');
+
+                return $this->redirectToRoute('profile_index');
+            }
+            else
+                $this->addFlash('danger', 'Les mots de passe ne correspondent pas');
+        }
+
+        return $this->render('profile/editpassword.html.twig', [
         ]);
     }
 
